953307daef9a2b5ab5fcfb9a61663ca62ee9cebd
Zaquiel <infozaquiel@gmail.com>
2020-11-24 12:37:36 +0100

    hola

--- server.js
+++ server.js
@@ -1,23 +1,42 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const cors = require("cors");
-
+const welcomeMessage = require("./welcomeMessage.json");
 const app = express();
 
+const fs = require("fs");
+
 app.use(cors());
+app.use(bodyParser.json());
 
-const welcomeMessage = {
-  id: 0,
-  from: "Bart",
-  text: "Welcome to CYF chat system!",
-};
-
-//This array is our "data store".
-//We will start with one message in the array.
-//Note: messages will be lost when Glitch restarts our server.
-const messages = [welcomeMessage];
+// //This array is our "data store".
+// //We will start with one message in the array.
+// //Note: messages will be lost when Glitch restarts our server.
 
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
 
-app.listen(process.env.PORT);
+app.get("/welcome", function (request, response) {
+  response.send(welcomeMessage);
+});
+
+//POST album
+app.post("/welcome/post", (req, res) => {
+  const postContent = req.body;
+  console.log(postContent);
+  const filterMessage = welcomeMessage.filter((el) => el.id === postContent.id);
+  const newMessage = welcomeMessage;
+  newMessage.push(postContent);
+  fs.writeFileSync("./welcomeMessage.json", JSON.stringify(newMessage));
+  console.log(newMessage);
+  res.send(postContent);
+});
+
+// // app.get("/welcome/:id", function (request, response) {
+// //   const id = request.params.id;
+// // });
+
+app.listen(3000, () => {
+  console.log("welcomed");
+});
